e605435790be502af535f94f8787f3bc3d314adc
GoToSchoolTeams <schoolgoto@gmail.com>
2024-06-04 13:25:03 +0900

    fix

---
+++ 0.projects/unity2dEntry/Assets/0_Gravity/StandardGravity.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Gravity2*/
+//物理の式に沿った正しい重力を作ります。
+//・物体式での重力の理解
+//・時間の導入(Fixedupdate())
+//・速度と加速度の導入
+//をやってみます。
+
+public class StandardGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
